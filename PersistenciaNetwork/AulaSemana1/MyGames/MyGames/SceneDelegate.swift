0986882eedd4ccb69730915f7222a5f50b63c9f3
Andre Erminio <andre.erminio@gmail.com>
2020-06-04 23:59:52 -0300

    atualizando

--- PersistenciaNetwork/AulaSemana1/MyGames/MyGames/SceneDelegate.swift
+++ PersistenciaNetwork/AulaSemana1/MyGames/MyGames/SceneDelegate.swift
@@ -29,6 +29,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
 
+    
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
